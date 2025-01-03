c180dea0374c4d2e5121e8875b8116f1acef4e46
Ljupce Dimitriev <ljupce@halicea.com>
2025-01-03 14:48:47 +0100

    Move virtual machine to separate tf file

--- azure.tf
+++ azure.tf
@@ -78,29 +78,3 @@ resource "azurerm_network_interface" "nic" {
     environment = "dev"
   }
 }
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = "test-dev-vm"
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = azurerm_resource_group.rg.location
-  size                  = "Standard_F2"
-  admin_username        = "adminuser"
-  network_interface_ids = [azurerm_network_interface.nic.id]
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/devazurekey.pub")
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-}
---
+++ virtual_machine.tf
@@ -0,0 +1,25 @@
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "test-dev-vm"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/devazurekey.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
