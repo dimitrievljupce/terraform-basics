77828f1cc1c736f068aa479f07d4c722cc6e573e
Ljupce Dimitriev <ljupce@halicea.com>
2025-01-03 11:08:38 +0100

    Add network interface

--- azure.tf
+++ azure.tf
@@ -61,3 +61,20 @@ resource "azurerm_public_ip" "publicip" {
     environment = "dev"
   }
 }
+
+resource "azurerm_network_interface" "nic" {
+  name                = "test-dev-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sn.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
