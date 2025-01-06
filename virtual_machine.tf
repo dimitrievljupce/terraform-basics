62f250840d3b1a84e1b261893fb163af3649238c
Ljupce Dimitriev <ljupce@halicea.com>
2025-01-06 15:36:46 +0100

    Install initial vm dependencies

--- virtual_machine.tf
+++ virtual_machine.tf
@@ -6,6 +6,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
   admin_username        = "adminuser"
   network_interface_ids = [azurerm_network_interface.nic.id]
 
+  custom_data = filebase64("./templates/dependencies.tpl")
+
   admin_ssh_key {
     username   = "adminuser"
     public_key = file("~/.ssh/devazurekey.pub")
